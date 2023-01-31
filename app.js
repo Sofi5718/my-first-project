7079938bb984964119ede3b0fa90e0be1ac69be6
Sofie Larsen <Sofieamalierl@hotmail.com>
2023-01-31 13:38:18 +0100

    flere små nye linjer af kode jeg ikke forstår

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-document.body.innerHTML += "Hello JavaScript!";
+//document.body.innerHTML += "Hello JavaScript!";
 //alert("Hello Dat.js");
 console.log("Hello Dat.js");
 console.log("Hi Students!");
 console.log("This is your first JavaScript App 🎉");
+
+function Sayhello() {
+  document.querySelector("h3").textContent =
+    "Hej " + document.querySelector("input").value;
+}
+document.querySelector("button").addEventListener("click", Sayhello);
+
+function Sayhello() {
+  document.querySelector("h5").textContent =
+    document.querySelector("input").value +
+    ", det er faktisk min ynglingsfarve";
+}
+document.querySelector("button").addEventListener("click", Sayhello);
